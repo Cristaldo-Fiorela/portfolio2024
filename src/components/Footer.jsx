5d5f7596818979a8ec51ae36d68b57bbf5b1edb4
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-29 20:06:11 -0300

    diseño inicial de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section>
+        <h2>Te gusta mi trabajo? Conectemos!</h2>
+        <div>
+          <a href="mailto:cristaldofiorela@gmail.com">cristaldofiorela@gmail.com</a>
+          <button>COPIAR</button>
+        </div>
+      </section>
+
+      <div>
+        <button>LINKEDIN</button>
+        <button>GITHUB</button>
+      </div>
+    </>
   )
 }
 
