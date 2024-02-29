2db938f30a644cb9e0e237641522e1770d489b31
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-29 19:52:04 -0300

    correcion texto about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
       <h2>About</h2>
       <div>
         <img src="" alt="" />
-        <p>Siempre fui una persona que posee tanto el lado creativo como el lógico. En 2020 descubrí el mundo de desarrollo web y me di cuenta de que podría ser un trabajo perfecto para mi permitiéndome usar mi lado creativo para diseñar y mi lado lógico para codear. Esto me permite ser capaz de llevar un proyecto desde 0 a la realidad y saber mis alcances y limitaciones, sin perder detalles en el camino.</p>
+        <p>Soy una persona con un equilibrio entre creatividad y lógica. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica en la codificación. Estoy comprometido/a a llevar proyectos desde su concepción hasta la realidad, comprendiendo mis capacidades y limitaciones en el proceso.</p>
         <p>Actualmente me encuentro realizando una tecnicatura superior en programación e innovación tecnológica y trabaje como mentora FrontEnd para Argentina Programa 4.0.</p>
       </div>
     </>
