f0859421fff4a6318c14d9116752d5501b9f1393
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-29 19:57:07 -0300

    composicion base de componente Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,29 @@
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+      <h2>Habilidades</h2>
+      <section>
+        <h3>Desarrollo Frontend</h3>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>TyperScript</p>
+        <p>React.js</p>
+        <p>React Native</p>
+      </section>
+
+      <section>
+        <h3>Desarrollo Backend</h3>
+        <p>Node.js</p>
+        <p>MongoDB</p>
+      </section>
+
+      <section>
+        <h3>Control de versiones</h3>
+        <p>Git</p>
+        <p>GitHub</p>
+      </section>
+    </>
   )
 }
 
