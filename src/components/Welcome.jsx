3b6a8e235b0bcb04f46ba07ecbb300bf8251f121
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-28 22:04:33 -0300

    estructura inicial de welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,29 @@
+import { Icon } from '@iconify/react';
+
+
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <>
+      <h2>Hola {""}
+        <span>
+          <Icon icon="mdi:hand-wave" width="2rem" height="2rem" />
+        </span>
+        {""} soy
+      </h2>
+      <h1>Fiorela Cristaldo</h1>
+      <div>
+        <h3>
+          desarrolladora {""}
+          <span>fullstack</span>
+        </h3>
+        <h3>
+          con pasión por el {""}        
+        <Icon icon="lucide:sparkle" width="1.2rem" height="1.2rem" />
+          {""} frontend {""}
+          <Icon icon="lucide:sparkle" width="1.2rem" height="1.2rem" />
+        </h3>
+      </div>
+    </>
   )
 }
 
