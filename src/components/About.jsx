385c76cff6c28e121f4502f7941d1291c1535865
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-29 23:39:15 -0300

    correccion de idioma

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import portrait from '../assets/image/portrait.jpeg';
 const About = () => {
   return (
     <>
-      <h2>About</h2>
+      <h2>Sobre Mi</h2>
       <div>
         <img src={portrait} alt="retrato" id='portrait'/>
         <p>Soy una persona con un equilibrio entre creatividad y lógica. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica en la codificación. Estoy comprometido/a a llevar proyectos desde su concepción hasta la realidad, comprendiendo mis capacidades y limitaciones en el proceso.</p>
