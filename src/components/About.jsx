674f5506df53d8607e55b170481ec2f516ff0cd2
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-03-08 20:05:01 -0300

    cambio de segundo parrafo de about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <img src={portrait} alt="retrato" id='portrait'/>
         <div className='about-description'>
           <p>Soy una persona con un equilibrio entre creatividad y lógica. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica en la codificación. Estoy comprometido/a a llevar proyectos desde su concepción hasta la realidad, comprendiendo mis capacidades y limitaciones en el proceso.</p>
-          <p>Actualmente me encuentro realizando una tecnicatura superior en programación e innovación tecnológica y trabaje como mentora FrontEnd para Argentina Programa 4.0.</p>
+          <p>Actualmente estoy cursando una Tecnicatura Superior en Programación e Innovación Tecnológica, y he tenido la oportunidad de desempeñarme como mentora FrontEnd para Argentina Programa 4.0.</p>
         </div>
       </div>
     </section>
