70f1f8ce7d669b8ff55c1f86e88639ae7142fe59
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-03-11 20:41:55 -0300

    resaltando palabras en texto about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
       <div>
         <img src={portrait} alt="retrato" id='portrait'/>
         <div className='about-description'>
-          <p>Soy una persona con un equilibrio entre creatividad y lógica. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica al codear.</p>
-          <p>Mi tiempo como Mentora FrontEnd profundice mis conocimientos tecnicos, puse en practica una comunicacion clara para lograr objetivos comunes y la resolucion de problemas.</p>
-          <p>Actualmente estoy cursando una Tecnicatura Superior en Programación e Innovación Tecnológica, y he tenido la oportunidad de desempeñarme como mentora FrontEnd para Argentina Programa 4.0.</p>
+          <p>Soy una persona con un equilibrio entre <span className='yellow-hl'>creatividad</span> y <span className='yellow-hl'>lógica</span>. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica al codear.</p>
+          <p>Mi tiempo como <span className='purple-hl'>Mentora FrontEnd</span> profundice mis conocimientos tecnicos, puse en practica una comunicacion clara para lograr objetivos comunes y la resolucion de problemas.</p>
+          <p>Actualmente estoy cursando una <span className='pink-hl'>Tecnicatura Superior en Programación e Innovación Tecnológica</span>, y he tenido la oportunidad de desempeñarme como mentora FrontEnd para Argentina Programa 4.0.</p>
         </div>
       </div>
     </section>
