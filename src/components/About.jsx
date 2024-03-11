d5b98ba47271c55648b077912e9eb3915e47a95d
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-03-11 15:17:40 -0300

    cambio de texto en about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,8 @@ const About = () => {
       <div>
         <img src={portrait} alt="retrato" id='portrait'/>
         <div className='about-description'>
-          <p>Soy una persona con un equilibrio entre creatividad y lógica. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica en la codificación. Estoy comprometido/a a llevar proyectos desde su concepción hasta la realidad, comprendiendo mis capacidades y limitaciones en el proceso.</p>
+          <p>Soy una persona con un equilibrio entre creatividad y lógica. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica al codear.</p>
+          <p>Mi tiempo como Mentora FrontEnd profundice mis conocimientos tecnicos, puse en practica una comunicacion clara para lograr objetivos comunes y la resolucion de problemas.</p>
           <p>Actualmente estoy cursando una Tecnicatura Superior en Programación e Innovación Tecnológica, y he tenido la oportunidad de desempeñarme como mentora FrontEnd para Argentina Programa 4.0.</p>
         </div>
       </div>
