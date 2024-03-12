a92ddc0a35bbee87b525fd4e2fab371d44f9786b
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-03-12 18:45:51 -0300

    estilos y animacion de botones de skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className='about-description'>
           <p>Soy una persona con un equilibrio entre <span className='yellow-hl'>creatividad</span> y <span className='yellow-hl'>lógica</span>. Descubrí mi pasión por el desarrollo web en 2020, donde encontré la combinación perfecta para utilizar tanto mi lado creativo en el diseño como mi habilidad lógica al codear.</p>
           <p>Mi tiempo como <span className='purple-hl'>Mentora FrontEnd</span> profundizó mis conocimientos técnicos, puso en práctica una comunicacion clara para lograr objetivos comunes y la resolución de problemas.</p>
-          <p>Actualmente estoy cursando una <span className='pink-hl'>Tecnicatura Superior en Programación e Innovación Tecnológica</span>, y he tenido la oportunidad de desempeñarme como mentora FrontEnd para Argentina Programa 4.0.</p>
+          <p>Actualmente estoy cursando una <span className='pink-hl'>Tecnicatura Superior en Programación e Innovación Tecnológica</span>.</p>
         </div>
       </div>
     </section>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,12 +8,12 @@ const Skills = () => {
         <section id="skill-front">
           <h3>Desarrollo Frontend</h3>
           <div className="skill-name">
-            <p>HTML <Icon icon="skill-icons:html" width="1.2rem" height="1.2rem" /> </p>
-            <p>CSS <Icon icon="skill-icons:css" width="1.2rem" height="1.2rem" /></p>
-            <p>JavaScript <Icon icon="skill-icons:javascript" width="1.2rem" height="1.2rem" /></p>
-            <p>TyperScript <Icon icon="skill-icons:typescript" width="1.2rem" height="1.2rem" /></p>
-            <p>React.js <Icon icon="logos:react" width="1.2rem" height="1.2rem" /></p>
-            <p>React Native <Icon icon="logos:react" width="1.2rem" height="1.2rem" /></p>
+            <p>HTML <Icon icon="skill-icons:html" width="1.7rem" height="1.7rem" /> </p>
+            <p>CSS <Icon icon="skill-icons:css" width="1.7rem" height="1.7rem" /></p>
+            <p>JavaScript <Icon icon="skill-icons:javascript" width="1.7rem" height="1.7rem" /></p>
+            <p>TyperScript <Icon icon="skill-icons:typescript" width="1.7rem" height="1.7rem" /></p>
+            <p>React.js <Icon icon="logos:react" width="1.7rem" height="1.7rem" /></p>
+            <p>React Native <Icon icon="logos:react" width="1.7rem" height="1.7rem" /></p>
           </div>
         </section>
 
@@ -21,16 +21,16 @@ const Skills = () => {
           <section id="skill-back">
             <h3>Desarrollo Backend</h3>
             <div className='skill-name'>
-              <p>Node.js <Icon icon="devicon:nodejs" width="1.2rem" height="1.2rem" /></p>
-              <p>MongoDB <Icon icon="vscode-icons:file-type-mongo" width="1.2rem" height="1.2rem" /></p>
+              <p>Node.js <Icon icon="devicon:nodejs" width="1.7rem" height="1.7rem" /></p>
+              <p>MongoDB <Icon icon="vscode-icons:file-type-mongo" width="1.7rem" height="1.7rem" /></p>
             </div>
           </section>
 
           <section id="skill-ver">
             <h3>Control de versiones</h3>
             <div className='skill-name'>
-              <p>Git <Icon icon="devicon:git" width="1.2rem" height="1.2rem" /></p>
-              <p>GitHub <Icon icon="fluent-mdl2:git-hub-logo" width="1.2rem" height="1.2rem" /></p>
+              <p>Git <Icon icon="devicon:git" width="1.7rem" height="1.7rem" /></p>
+              <p>GitHub <Icon icon="fluent-mdl2:git-hub-logo" width="1.7rem" height="1.7rem" /></p>
             </div>
           </section>
         </div>
