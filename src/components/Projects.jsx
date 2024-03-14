04b912ccb69158e96002e52f72224666a60a8739
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-03-13 21:55:31 -0300

    cambio de clases de contenedor de proyecto

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,22 +4,23 @@ const Projects = () => {
   return (
     <section id="projects">
       <h2 className="section-title">Proyectos</h2>
-      { DB.map(project => (
-        <div 
-          className="project-container"
-          key={project.id}
-          style={{backgroundImage: `url('${project.image}')`}}
-        >
-          <div className="tech-tags">
-            {project.tech.map( (tech, i) => (
-              <p key={i}>#{tech}</p>
-            ))}
+      <div className='projects-container'>
+        { DB.map(project => (
+          <div 
+            className="project-card"
+            key={project.id}
+            style={{backgroundImage: `url('${project.image}')`}}
+          >
+            <div className="tech-tags">
+              {project.tech.map( (tech, i) => (
+                <p key={i}>#{tech}</p>
+              ))}
+            </div>
+            <h4>{project.name}</h4>
           </div>
-          <h4>{project.name}</h4>
-        </div>
-        ))
-      }
-
+          ))
+        }
+      </div>
     </section>
   )
 }
