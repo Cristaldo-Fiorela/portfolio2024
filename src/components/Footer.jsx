5183b92e7348636dfcd42d9c87aafc0ca186b2ca
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-03-25 23:27:35 -0300

    agregando efecto de iconos al hacer una copia exitosa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,30 @@ const Footer = () => {
 
   const [copy, setCopy] = useState(false);
 
+  const copied = () => {
+    setCopy(true);
+
+    setTimeout(() => {
+      setCopy(false);
+    }, 1000);
+  }
+
   return (
     <footer>
       <section id="contact-container">
         <h2>Te gusta mi trabajo? Conectemos!</h2>
         <div id='mail-box'>
           <a href="mailto:cristaldofiorela@gmail.com">cristaldofiorela@gmail.com</a>
-          <CopyToClipboard text={'cristaldofiorela@gmail.com'}>
+          <CopyToClipboard 
+            text={'cristaldofiorela@gmail.com'}
+            onCopy={copied}
+          >
             <button title='copiar'>
-              <Icon icon="lucide:copy-plus" width="2rem" height="2rem" />
+              {copy ? 
+                <Icon icon="tabler:copy-check" width="2rem" height="2rem" />              
+                :
+                <Icon icon="lucide:copy-plus" width="2rem" height="2rem" />
+              }
             </button>
           </CopyToClipboard>
         </div>
