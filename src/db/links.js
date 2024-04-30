00064ed883cc812e92b9ea338c82b2daec8b67c9
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-04-30 16:28:39 -0300

    cambio de links ed logo

--- src/db/links.js
+++ src/db/links.js
@@ -1,6 +1,6 @@
 const cvSpanish = 'https://drive.google.com/file/d/1c6gwb21RlpnsBcyR3tWmsWw35ITt7RGp/view?usp=sharing';
-const logo = 'https://i.imgur.com/UXWYjev.png';
-const logoWhite = 'https://i.imgur.com/1tbHStn.png';
+const logo = 'https://i.imgur.com/yasyMtb.png';
+const logoWhite = 'https://i.imgur.com/Ik3YTTD.png';
 
 export {
   cvSpanish,
